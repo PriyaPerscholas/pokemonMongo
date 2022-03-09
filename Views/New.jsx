44b7791e0c61c6ea2ba9263cf3be24ca66e81e5b
unknown <PriyaS@WI-CCP-ST-04.wilkescc.edu>
2022-03-09 15:19:45 -0500

    css update

--- Views/New.jsx
+++ Views/New.jsx
@@ -1,9 +1,14 @@
 const React = require('react')
+const myStyle = {
+       color: 'black',
+       backgroundColor: 'lightcoral',
 
+
+}
 class New extends React.Component {
        render() {
               return (
-                     <div>
+                     <div style={myStyle}>
                             <h1>New Pokemon Page</h1>
                             <form action="/pokemon" method="POST">
                                    Pokemon Name: <input type="text" name="name" />
--- Views/Show.jsx
+++ Views/Show.jsx
@@ -1,15 +1,19 @@
 
 const React = require("react")
+const myStyle = {
+       color: 'black',
+       backgroundColor: 'lightcoral',
+       minheight: '100vh',
+       maxwidth: '400px',
+       margin: 0,
+
+}
 class Show extends React.Component {
        render() {
               const { pokemon } = this.props;
 
               return (
-
-                     // <head>
-                     //        <title>Pokemon</title>
-                     // </head>
-                     <body style={{ backgroundColor: 'lightcoral' }}>
+                     <body style={myStyle} >
                             <h1 >'Gotta Catch 'Em All'</h1>
                             <h2>{pokemon.name.charAt(0).toUpperCase(0) + pokemon.name.slice(1)}</h2>
                             <img src={`${pokemon.img}.jpg`}></img>
